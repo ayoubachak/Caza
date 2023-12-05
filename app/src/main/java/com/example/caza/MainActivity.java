65a3810152d628d5a12e49be13194845805d1125
Ayoub Achak <ayoub.achak01@gmail.com>
2023-12-05 12:24:23 +0100

    Small change to test the collaboration

--- app/src/main/java/com/example/caza/MainActivity.java
+++ app/src/main/java/com/example/caza/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.caza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
 
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
